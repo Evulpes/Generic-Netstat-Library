c4b10f9e9f82ca85cad230f8b61aafff4bf5a7a1
Evulpes <54101182+Evulpes@users.noreply.github.com>
2020-04-28 17:04:17 +0100

    Add project files.

---
+++ NSLib.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace Generic_Netstat_Library
+{
+    public class NSLib : NativeMethods
+    {
+        private delegate WinError.SeverityCode GetTableMethod(out IntPtr tableType);
+
+        
+        public static dynamic GetExtendedTable(out IntPtr tablePtr, TableType tableType = TableType.TCP)
+        {
+            //Create an empty delegate for the TCP, or UDP, query.
+            GetTableMethod GetTable;
+            dynamic tabelRowArray;
+            dynamic tableInfo;
+
+            //If the tableType is UDP, initialize the dynamic values and delegate to UDP values.
+            if (tableType == TableType.UDP)
+            {
+                GetTable = new GetTableMethod(ConnectionTables.GetExtendedUdpTable);
+                tableInfo = new Udpmib.MIB_UDPTABLE_OWNER_PID();
+                tabelRowArray = new Udpmib.MIB_UDPROW_OWNER_PID();
+            }
+            else
+            {
+                GetTable = new GetTableMethod(ConnectionTables.GetExtendedTcpTable);
+                tableInfo = new Tcpmib.MIB_TCPTABLE_OWNER_PID();
+                tabelRowArray = new Tcpmib.MIB_TCPROW_OWNER_PID();
+            }
+
+            WinError.SeverityCode status = GetTable(out tablePtr);
+            if (status != WinError.SeverityCode.NO_ERROR)
+                throw new Exception($"Failed on GetExtendedTable ({tableType}). Error Code: {status}.");
+
+            //Marshals the table pointer to either a TCP or UDP table struct.
+            tableInfo = Convert.ChangeType(Marshal.PtrToStructure(tablePtr, (Type)tableInfo.GetType()), tableInfo.GetType());
+
+            //Initialize a new array of either a TCP or UDP row array.
+            tabelRowArray = Array.CreateInstance(tabelRowArray.GetType(), tableInfo.dwNumEntries);
+
+            //Gets a pointer to the first row in the table.
+            IntPtr rowPtr = (IntPtr)(tablePtr + Marshal.SizeOf(tableInfo.dwNumEntries));
+
+
+            for (int i = 0; i < tabelRowArray.Length; i++)
+            {
+                //Marshals the index of the table row array to the content of row struct.
+                tabelRowArray[i] = Convert.ChangeType(Marshal.PtrToStructure(rowPtr, (Type)tabelRowArray[i].GetType()), tabelRowArray[i].GetType());
+
+                //Increments the pointer to the row by the size of the struct.
+                rowPtr = (IntPtr)((long)rowPtr + Marshal.SizeOf(tabelRowArray[i]));
+
+                //If the below try/catch fails, the process has likely terminated since.
+                string processName;
+                try
+                {
+                    processName = Process.GetProcessById(tabelRowArray[i].dwOwningPid).ProcessName;
+                }
+                catch
+                {
+                    continue;
+                }
+
+                
+
+            }
+            return tabelRowArray;
+        }
+        public static WinError.SeverityCode CloseConnection(Tcpmib.MIB_TCPROW_OWNER_PID tcpTableRow)
+        {
+            tcpTableRow.dwState = Tcpmib.TCPState.MIB_TCP_STATE_DELETE_TCB;
+            IntPtr tcpRowPtr = Marshal.AllocHGlobal(Marshal.SizeOf(tcpTableRow));
+            Marshal.StructureToPtr(tcpTableRow, tcpRowPtr, false);
+
+            WinError.SeverityCode ntStatus = Iphlpapi.SetTcpEntry(tcpRowPtr);
+
+            Marshal.FreeHGlobal(tcpRowPtr);
+
+            return ntStatus;
+        }
+        public enum TableType
+        {
+            /// <summary>
+            /// If the type of query required is TCP.
+            /// </summary>
+            TCP,
+            /// <summary>
+            /// If the type of query required is UDP.
+            /// </summary>
+            UDP,
+        }
+
+    }
+    class ConnectionTables : NativeMethods
+    {
+        internal static WinError.SeverityCode GetExtendedTcpTable(out IntPtr tcpTable)
+        {
+            tcpTable = IntPtr.Zero;
+            //The initial status will return as ERROR_INSUFFICIENT_BUFFER, and pdwSize will contain the correct buffer size.
+            WinError.SeverityCode status = Iphlpapi.GetExtendedTcpTable(IntPtr.Zero, out int pdwSize, true, Winsock.AddressFamily.AF_INET, Iprtrmib.TCP_TABLE_CLASS.TCP_TABLE_OWNER_PID_ALL);
+
+            //The buffer may be slightly higher after the previous statement, so check.
+            while (status == WinError.SeverityCode.ERROR_INSUFFICIENT_BUFFER)
+            {
+                if (tcpTable != IntPtr.Zero)
+                    Marshal.FreeHGlobal(tcpTable);
+                tcpTable = Marshal.AllocHGlobal(pdwSize);
+                status = Iphlpapi.GetExtendedTcpTable(tcpTable, out pdwSize, true, Winsock.AddressFamily.AF_INET, Iprtrmib.TCP_TABLE_CLASS.TCP_TABLE_OWNER_PID_ALL);
+            }
+            return status;
+        }
+        internal static WinError.SeverityCode GetExtendedUdpTable(out IntPtr udpTable)
+        {
+            udpTable = IntPtr.Zero;
+            WinError.SeverityCode status = Iphlpapi.GetExtendedUdpTable(IntPtr.Zero, out int pdwSize, true, Winsock.AddressFamily.AF_INET, Iprtrmib.UDP_TABLE_CLASS.UDP_TABLE_OWNER_PID);
+            while (status == WinError.SeverityCode.ERROR_INSUFFICIENT_BUFFER)
+            {
+                if (udpTable != IntPtr.Zero)
+                    Marshal.FreeHGlobal(udpTable);
+
+                udpTable = Marshal.AllocHGlobal(pdwSize);
+                status = Iphlpapi.GetExtendedUdpTable(udpTable, out pdwSize, true, Winsock.AddressFamily.AF_INET, Iprtrmib.UDP_TABLE_CLASS.UDP_TABLE_OWNER_PID);
+            }
+            return status;
+        }
+    }
+    public static class Extensions
+    {
+        /// <summary>
+        /// Swaps the first two bytes of an array.
+        /// </summary>
+        /// <param name="array">The array on which to perform the swap.</param>
+        /// <returns>A new byte[].</returns>
+        public static int ToPort(this byte[] array) => array.Length < 0 ? 0 : BitConverter.ToUInt16(new byte[2] { array[1], array[0], }, 0);
+    }
+
+}
---
+++ NativeMethods.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generic_Netstat_Library
+{
+    public class NativeMethods
+    {
+        protected class Iphlpapi : WinError
+        {
+            [DllImport("iphlpapi.dll", SetLastError = true)]
+            public static extern SeverityCode GetExtendedTcpTable(IntPtr pTcpTable, out int pdwSize, bool bOrder, Winsock.AddressFamily ulAf, Iprtrmib.TCP_TABLE_CLASS TableClass, int Reserved = 0);
+            [DllImport("iphlpapi.dll", SetLastError = true)]
+            public static extern SeverityCode GetExtendedUdpTable(IntPtr pUdpTable, out int pdwSize, bool bOrder, Winsock.AddressFamily ulAf, Iprtrmib.UDP_TABLE_CLASS TableClass, int Reserved = 0);
+            [DllImport("iphlpapi.dll", SetLastError = true)]
+            public static extern SeverityCode SetTcpEntry(IntPtr pTcprow);
+        }
+        public static class Iprtrmib
+        {
+            public enum TCP_TABLE_CLASS
+            {
+                TCP_TABLE_BASIC_LISTENER,
+                TCP_TABLE_BASIC_CONNECTIONS,
+                TCP_TABLE_BASIC_ALL,
+                TCP_TABLE_OWNER_PID_LISTENER,
+                TCP_TABLE_OWNER_PID_CONNECTIONS,
+                TCP_TABLE_OWNER_PID_ALL,
+                TCP_TABLE_OWNER_MODULE_LISTENER,
+                TCP_TABLE_OWNER_MODULE_CONNECTIONS,
+                TCP_TABLE_OWNER_MODULE_ALL
+            }
+            public enum UDP_TABLE_CLASS
+            {
+                UDP_TABLE_BASIC,
+                UDP_TABLE_OWNER_PID,
+                UDP_TABLE_OWNER_MODULE
+            }
+        }
+        public static class Tcpmib
+        {
+
+            public struct MIB_TCPTABLE_OWNER_PID
+            {
+                public int dwNumEntries;
+                [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct, SizeConst = 1)]
+                public MIB_TCPROW_OWNER_PID[] table;
+            }
+            public struct MIB_TCPROW_OWNER_PID
+            {
+                public Tcpmib.TCPState dwState;
+                public int dwLocalAddr;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+                public byte[] dwLocalPort;
+                public int dwRemoteAddr;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+                public byte[] dwRemotePort;
+                public int dwOwningPid;
+            }
+            public enum TCPState
+            {
+                MIB_TCP_STATE_CLOSED = 1,
+                MIB_TCP_STATE_LISTEN = 2,
+                MIB_TCP_STATE_SYN_SENT = 3,
+                MIB_TCP_STATE_SYN_RCVD = 4,
+                MIB_TCP_STATE_ESTAB = 5,
+                MIB_TCP_STATE_FIN_WAIT1 = 6,
+                MIB_TCP_STATE_FIN_WAIT2 = 7,
+                MIB_TCP_STATE_CLOSE_WAIT = 8,
+                MIB_TCP_STATE_CLOSING = 9,
+                MIB_TCP_STATE_LAST_ACK = 10,
+                MIB_TCP_STATE_TIME_WAIT = 11,
+                MIB_TCP_STATE_DELETE_TCB = 12,
+                MIB_TCP_STATE_RESERVED = 100
+            }
+        }
+        public static class Udpmib
+        {
+            public struct MIB_UDPTABLE_OWNER_PID
+            {
+                public uint dwNumEntries;
+                [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct, SizeConst = 1)]
+                public MIB_UDPROW_OWNER_PID[] table;
+            }
+            public struct MIB_UDPROW_OWNER_PID
+            {
+                public uint dwLocalAddr;
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+                public byte[] dwLocalPort;
+                public int dwOwningPid; //uint?
+            }
+        }
+        public class WinError
+        {
+            public enum SeverityCode : uint
+            {
+                ERROR_SUCCESS = 0x0,
+                NO_ERROR = 0x0,
+                ERROR_NOT_SUPPORTED = 0x32,
+                ERROR_INVALID_PARAMETER = 0x57,
+                ERROR_INSUFFICIENT_BUFFER = 0x7A,
+            }
+        }
+        public static class Winsock
+        {
+            public enum AddressFamily
+            {
+                AF_INET = 2,
+            }
+        }
+    }
+}
